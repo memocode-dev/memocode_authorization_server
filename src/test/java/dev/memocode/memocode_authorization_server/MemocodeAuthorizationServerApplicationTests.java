1cddd811eefe1914937af94e2896ec98f85cd4ab
audgks5551 <audgsk5551@daum.net>
2024-03-11 02:13:07 +0900

    [update] 테스트케이스에서 임베디드 카프카를 사용할 수 있도록 수정

--- src/test/java/dev/memocode/memocode_authorization_server/MemocodeAuthorizationServerApplicationTests.java
+++ src/test/java/dev/memocode/memocode_authorization_server/MemocodeAuthorizationServerApplicationTests.java
@@ -2,8 +2,16 @@ package dev.memocode.memocode_authorization_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
 
+@EmbeddedKafka(
+        partitions = 1,
+        brokerProperties = {
+                "listeners=PLAINTEXT://localhost:9092"
+        },
+        ports = {9092}
+)
 @SpringBootTest
 @ActiveProfiles("test")
 class MemocodeAuthorizationServerApplicationTests {
